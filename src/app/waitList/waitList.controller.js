784ecab17091d9b7f0b003adcc2092a56508b916
Timothy Taylor <ttaylor272@gmail.com>
2016-05-06 19:35:31 -0400

    save

---
+++ src/app/waitList/waitList.controller.js
@@ -0,0 +1,19 @@
+(function() {
+    'use strict';
+    
+    angular
+    .module('app.waitList')
+    .controller('WaitListController', WaitListController);
+    
+    function WaitListController() {
+        var vm = this;
+        
+        vm.parties = [1,2,3,4];
+        
+        vm.addParty = addParty;
+        
+        function addParty() {
+            vm.parties.push('another');
+        }
+    }
+}) ();
